d65fde319774ac7c07e24ba180a1fd40482334b1
smartsky97 <121670142@qq.com>
2020-05-25 22:34:53 +0800

    v1.0

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
     // just master
     // just test1
+    // v1.0
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
